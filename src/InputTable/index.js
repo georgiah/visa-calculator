4337f1ecf58a8a4323968ada96a5f7a1de218a4b
georgiah <georgia+github@fastmail.com>
2018-01-12 15:03:49 +0100

    Updating date fields stored in state

--- src/InputTable/index.js
+++ src/InputTable/index.js
@@ -5,32 +5,40 @@ export default class InputTable extends React.Component {
   render () {
     return (
       <table>
-        <tr>
-          <th>Remove</th>
-          <th>Entry Date</th>
-          <th>Exit Date</th>
-          <th>Country</th>
-          <th># Days</th>
-        </tr>
-        <tr>
-          <td>
-            <button>X</button>
-          </td>
-          <td>
-            <input type="date" />
-          </td>
-          <td>
-            <input type="date" />
-          </td>
-          <td>
-            <select name="country">
-              <option value="Europe" selected disabled>Europe</option>
-              <option value="Germany">Germany</option>
-              <option value="Italy">Italy</option>
-            </select>
-          </td>
-          <td>XX of XX</td>
-        </tr>
+        <thead>
+          <tr>
+            <th>Remove</th>
+            <th>Entry Date</th>
+            <th>Exit Date</th>
+            <th>Country</th>
+            <th># Days</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <button>X</button>
+            </td>
+            <td>
+              <input type="date" name="entry-0"
+                onChange={this.props.onChange}
+              />
+            </td>
+            <td>
+              <input type="date" name="exit-0"
+                onChange={this.props.onChange}
+              />
+            </td>
+            <td>
+              <select name="country">
+                <option value="Europe" defaultValue disabled>Europe</option>
+                <option value="Germany">Germany</option>
+                <option value="Italy">Italy</option>
+              </select>
+            </td>
+            <td>XX of XX</td>
+          </tr>
+        </tbody>
       </table>
     )
   }
--- src/index.js
+++ src/index.js
@@ -5,12 +5,38 @@ import Banner from './Banner'
 import InputTable from './InputTable'
 
 class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      entries: [{
+        entry: null,
+        exit: null,
+        country: null
+      }]
+    }
+  }
+
+  handleChange (e) {
+    var entries = this.state.entries
+    const name = e.target.name
+    const row = parseInt(name.substring(name.indexOf('-') + 1), 10)
+    const parameter = name.substring(0, name.indexOf('-'))
+
+    entries[row][parameter] = e.target.value
+
+    this.setState({
+      entries: entries
+    })
+  }
+
   render () {
     return (
-      <container class="app">
+      <div className="app">
         <Banner />
-        <InputTable />
-      </container>
+        <InputTable
+          onChange={(e) => this.handleChange(e)}
+        />
+      </div>
     )
   }
 }
