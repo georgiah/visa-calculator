f88bac88ef6e1e6c5e5f8ff3fc32c64c1a73c049
georgiah <georgia+github@fastmail.com>
2018-01-15 16:23:53 +0100

    Displays number of days per row

--- src/TableRow/index.js
+++ src/TableRow/index.js
@@ -33,6 +33,18 @@ export default class TableRow extends React.Component {
     }
   }
 
+  fullEntry (entry) {
+    return (entry.entry && entry.exit && entry.country)
+  }
+
+  numDays (day1, day2) {
+    const d1 = new Date(day1)
+    const d2 = new Date(day2)
+
+    const timeDiff = Math.abs(d2.getTime() - d1.getTime())
+    return Math.ceil(timeDiff / (1000 * 3600 * 24)) + 1
+  }
+
   render () {
     const index = this.props.index
     const entry = this.props.entry
@@ -67,7 +79,10 @@ export default class TableRow extends React.Component {
             })}
           </select>
         </td>
-        <td></td>
+        <td>
+          {this.fullEntry(entry) ?
+            `${this.numDays(entry.entry, entry.exit)} of XX` : ''}
+        </td>
         <td></td>
       </tr>
     )
